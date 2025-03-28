c0c8f18f3ceb7df73c8b621a2eebf9254eb9d204
Dilukshan <dilukshan2020da@gmail.com>
2025-03-28 19:03:31 +0530

    create app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+//create a endpoint
+app.get('/', (req, res) => {
+    res.send('CI/CD Pipeline with Jenkins and Docker for Node.js Application');
+})
+
+app.listen(4000,()=>{
+    console.log('Server is running on port 4000');
+})
