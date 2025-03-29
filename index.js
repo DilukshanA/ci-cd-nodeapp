89192eab804618f3dc4191651cd2c8f5615e4dc3
Dilukshan <dilukshan2020da@gmail.com>
2025-03-29 10:50:05 +0530

    test-1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 //create a endpoint
 app.get('/', (req, res) => {
-    res.send('CI/CD Pipeline with Jenkins and Docker for Node.js Application');
+    res.send('CI/CD Pipeline with Jenkins and Docker for Node.js Application -> test-1');
 })
 
 app.listen(4000,()=>{
